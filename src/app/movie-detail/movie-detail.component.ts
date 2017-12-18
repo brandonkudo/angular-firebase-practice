d0164d15599c8c95f818bb15ee83c1c3c60f99b3
Brandon Kudo <brandonkudo@gmail.com>
2017-12-17 20:28:36 -0800

    add service to details

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -2,21 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Movie } from '../movie.model';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  styleUrls: ['./movie-detail.component.scss'],
+  providers: [MovieService]
 })
 export class MovieDetailComponent implements OnInit {
   movieId: number = null;
+  movieToDisplay: Movie;
 
-  constructor(private route: ActivatedRoute, private location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.movieId = parseInt(urlParameters['id']);
     });
+  this.movieToDisplay = this.movieService.getMovieById(this.movieId);
   }
 
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -11,4 +11,12 @@ export class MovieService {
     return MOVIES;
   }
 
+  getMovieById(movieId: number) {
+    for (let i = 0; i <= MOVIES.length - 1; i++) {
+      if (MOVIES[i].id === movieId) {
+        return MOVIES[i];
+      }
+    }
+  }
+
 }
