67a37920a437319f220787b7ab8056435f93f848
Brandon Kudo <brandonkudo@gmail.com>
2017-12-17 18:50:15 -0800

    add detail page

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Movie } from '../movie.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-marketplace',
@@ -7,9 +9,14 @@ import { Movie } from '../movie.model';
   styleUrls: ['./marketplace.component.scss']
 })
 export class MarketplaceComponent {
+  constructor(private router: Router) {}
   movies: Movie[] = [
-    new Movie("Requiem for a Dream","David Lynch",2000),
-    new Movie("Mulholland Drive","David Lynch", 2005),
-    new Movie("The Departed","Martin Scorcese",2008)
+    new Movie("Requiem for a Dream","David Lynch",2000,1),
+    new Movie("Mulholland Drive","David Lynch", 2005,2),
+    new Movie("The Departed","Martin Scorcese",2008,3)
   ]
+
+  goToDetailPage(clickedMovie: Movie) {
+    this.router.navigate(['movies', clickedMovie.id]);
+  };
 }
